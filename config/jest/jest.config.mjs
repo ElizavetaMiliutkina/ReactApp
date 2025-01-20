713a126047aca17ffbe951db888ab9337694e9a7
liza <missis.liza-m@yandex.ru>
2025-01-20 12:24:30 +0300

    Jest test added

---
+++ config/jest/jest.config.mjs
@@ -0,0 +1,44 @@
+import path from 'path';
+
+export default {
+  clearMocks: true,
+  testEnvironment: 'jest-environment-jsdom',
+  coveragePathIgnorePatterns: [
+    '\\\\node_modules\\\\',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node',
+  ],
+  moduleDirectories: [
+    'node_modules',
+  ],
+  modulePaths: [
+    '<rootDir>src',
+  ],
+  testMatch: [
+    '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+  ],
+  rootDir: '../../',
+  transform: {
+    '\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          outDir: './.ts-jest',
+        },
+      },
+    ],
+    '\\.jsx?$': ['babel-jest', { plugins: ['@babel/plugin-transform-modules-commonjs'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
