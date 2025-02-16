9a3336fa6bacdbfa5096d29a3e9a1dfac0902ba6
liza <miss.lizamil@gmail.com>
2025-02-16 15:41:18 +0300

    errors fix

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -4,13 +4,8 @@ import * as path from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
+  stories: [path.resolve(__dirname, "../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)")],
+  addons: [],
   framework: {
     name: "@storybook/react-vite",
     options: {},
