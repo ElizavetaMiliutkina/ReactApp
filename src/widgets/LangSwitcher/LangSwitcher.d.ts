20deea7c9742e19c08c80842f6b74bc6fdd29259
liza <miss.lizamil@gmail.com>
2025-03-08 15:07:12 +0300

    added Login Form and Input with stories

--- src/widgets/LangSwitcher/LangSwitcher.d.ts
+++ src/widgets/LangSwitcher/LangSwitcher.d.ts
@@ -1,5 +1,6 @@
 interface LangSwitcherProps {
     className?: string;
+    short?: boolean;
 }
 export declare const LangSwitcher: (props: LangSwitcherProps) => import("react/jsx-runtime").JSX.Element;
 export {};
